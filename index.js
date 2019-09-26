f8538f6c2c8763b049936b159246f791c2489e1a
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2019-09-26 18:08:45 +0300

    Adding some basic error handling for niceness

--- index.js
+++ index.js
@@ -3,39 +3,48 @@
 const cfonts = require('cfonts')
 const boxen = require('boxen')
 const chalk = require('chalk')
+let username, jsonFile;
 
-const username = process.argv[2]
 
-const jsonFile = require(`./profiles/${username}`)
-
-console.log(username)
+try {
+    username = process.argv[2];
+    if (!username) {
+        console.error('You must specify a github username');
+    } else {
+        jsonFile = require(`./profiles/${username}`);
+        render();
+    }
+} catch (e) {
+    switch (e.code) {
+        case 'MODULE_NOT_FOUND':
+            console.error('The specified github username was not found');
+            break;
+        default:
+            console.error('An unknown error has occurred');
+            break;
+    }
+}
 
 /**
  * Renders an ascii art style name.
  */
-const showName = () => {
-    const bigName = cfonts.render(jsonFile.name, {
-        colors: ['yellow'],
-        font: 'simple',
-    })
-
-    return bigName.string
+function render() {
+    const showName = () => {
+        const bigName = cfonts.render(jsonFile.name, {
+            colors: ['yellow'],
+            font: 'simple',
+        });
+    
+        return bigName.string;
+    }
+    
+    const showLocation = () => chalk`{white Location:}    {cyan ${jsonFile['location']}}`;
+    
+    const showWebsite = () => chalk`{white Website:}     {cyan {underline ${jsonFile['website']}}}`;
+    
+    const showLinks = () => jsonFile.links.map(link => chalk`{grey ${link.label}:} {yellow ${link.url}}`).join('\n');
+    
+    const displayAll = () => `${showName()}\n\n${showLocation()}\n\n${showWebsite()}\n\n${showLinks()}`;
+    
+    console.log(boxen(displayAll(), { padding: 1, margin: 1, borderStyle: 'round' }));
 }
-
-const showLocation = () => chalk`{white Location:}    {cyan ${jsonFile['location']}}`
-
-const showWebsite = () => chalk`{white Website:}     {cyan {underline ${jsonFile['website']}}}`
-
-const showLinks = () => jsonFile.links.map(link => chalk`{grey ${link.label}:} {yellow ${link.url}}`).join('\n')
-
-const displayAll = () => `
-${showName()}
-
-${showLocation()}
-
-${showWebsite()}
-
-${showLinks()}
-`
-
-console.log(boxen(displayAll(), { padding: 1, margin: 1, borderStyle: 'round' }))
